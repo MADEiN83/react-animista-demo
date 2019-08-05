913f91106b3efe3884afe5c5c7b9f64744919e6e
Anthony DI STEFANO <ADIS@supralog.local>
2019-08-05 10:05:13 +0200

    Updates

---
+++ precache-manifest.7d7780fbb40c6fe81148edcaa07c7007.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36525fd669a26fe65f98c8ddae49454f",
+    "url": "/react-animista-demo/index.html"
+  },
+  {
+    "revision": "3bb2637310a1e078b515",
+    "url": "/react-animista-demo/static/css/main.3785946b.chunk.css"
+  },
+  {
+    "revision": "72c252c37735d2180df8",
+    "url": "/react-animista-demo/static/js/2.c2695c00.chunk.js"
+  },
+  {
+    "revision": "3bb2637310a1e078b515",
+    "url": "/react-animista-demo/static/js/main.a1e10d18.chunk.js"
+  },
+  {
+    "revision": "358dbb83876c18c556fa",
+    "url": "/react-animista-demo/static/js/runtime~main.fbab4a48.js"
+  }
+]);
